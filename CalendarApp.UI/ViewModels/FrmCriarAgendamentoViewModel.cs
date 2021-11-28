0ce542923596c54e0d5f591e6f4ebc3192b1846c
William Dutra <will.dutra26@gmail.com>
2021-11-28 17:08:37 -0300

    adicionado formulario de cadastramento de evento

---
+++ CalendarApp.UI/ViewModels/FrmCriarAgendamentoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarApp.UI.ViewModels
+{
+    public class FrmCriarAgendamentoViewModel : ViewModelBase
+    {
+
+        #region Binding Properties
+
+        private string _Nome;
+
+        public string Nome 
+        { 
+            get 
+            {
+                return _Nome;
+            }
+            set 
+            {
+                SetProperty(ref _Nome, value);
+            }
+        }
+
+        private string _Descricao;
+
+        public string Descricao 
+        { 
+            get
+            {
+                return _Descricao;
+            }
+            set
+            {
+                SetProperty(ref _Descricao, value);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ CalendarApp.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CalendarApp.UI.FRM;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarApp.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public Command Visualizar { get; set; }
+
+        public Command Cadastrar { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Cadastrar = new Command(() => new FrmCriarAgendamento().ShowDialog());
+        }
+
+    }
+}
