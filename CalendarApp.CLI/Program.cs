aa8a02494edf89ad89d0c602e90fe1280d567e0d
William Dutra <will.dutra26@gmail.com>
2021-12-04 19:43:03 -0300

    ajuste nas pasta da solucao

--- CalendarApp.CLI/Program.cs
+++ CalendarApp.CLI/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CalendarApp.App;
+using CalendarApp.App.Interfaces;
+using CalendarApp.Infra.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CalendarApp.CLI
 {
@@ -6,7 +10,39 @@ namespace CalendarApp.CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("INICIANDO EXECUÇÕES DE AGENDAMENTOS");
+
+            try
+            {
+
+                var serviceCollection = new ServiceCollection();
+                Startup.Register(serviceCollection);
+                var age = Startup.Container.GetService<IAgendamento>();
+                var prompt = Startup.Container.GetService<IPrompt>();
+
+                var agendamentos = age.ListarAgendamentoParaExecucao();
+
+                foreach (var agendamento in agendamentos)
+                {
+                    try
+                    {
+
+                        var args1 = agendamento.Argumento.Split(' ');
+
+                        prompt.Run(agendamento.Caminho, agendamento.Nome, args1, agendamento.Horario);
+
+                    }catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
         }
     }
 }
